bcfe6c3dc3807fdf5dc57a44218fb4b11e2a879a
Memebraindt <83882854+Memebraindt@users.noreply.github.com>
2025-02-24 23:12:36 +0300

    Add files via upload

--- 2_Make_a_spiral.py
+++ 2_Make_a_spiral.py
@@ -7,7 +7,7 @@ def check(n, nx, ny, matrix) -> int:
 def get_new_direction(d):
     return (d + 1) % 4
 
-def generate_spiral(n: int):
+def spiralize(n):
     matrix = [[0] * n for _ in range(n)]
     directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
     x, y, d, pd = 0, 0, 0, 1
@@ -16,29 +16,31 @@ def generate_spiral(n: int):
     while spiral_not_done:
         matrix[x][y] = 1
 
-        l1 = check(n, x + 2*dx, y + 2*dy, matrix)
+        l1 = check(n, x + 2*dx, y + 2*dy, matrix) # смотрим через 1 клетку в том же направлении
         nd = get_new_direction(d)
         ndx, ndy = directions[nd]
-        l2 = check(n, x + 2*ndx, y + 2*ndy, matrix)
+        l2 = check(n, x + 2*ndx, y + 2*ndy, matrix) # смотрим через одну клетку в новом направлении
 
-        if l1 == l2 == 0:   # значит, можно идти дальше
-            pass 
-        elif (l2 == 0 and l1 == 1) or check(n, x+dx, y+dy, matrix)==-1:       # если нужно повернуть
-            if pd == 1:
+        """ Возможные значения l1 и l2 соответственно:
+        [0, 0] - можно идти дальше
+        [1, 0] - произойдёт самопересечение если продолжим двигаться дальше
+        [-1 0] - выйдем за край через 2 хода
+        [1 1] - дальше некуда идти. конец.
+        check == -1 проверка на выход за край """
+        if (l1 == 1 and l2 == 0) or check(n, x+dx, y+dy, matrix)==-1:
+            if pd == 1:                             # если за два хода нужно сделать два поворота, то получается самопересечение, дальше не идём
                 spiral_not_done = False    
             pd = 0
             d = get_new_direction(d)
             dx, dy = directions[d]
-        elif l1 == -1 and l2 == 0:
-            pass 
-        else:               # l1 == l2 == 1 или другие значения говорят о том, что спираль готова
+        elif l1 == l2 == 1 or matrix[x+dx][y+dy] == 1:               
             spiral_not_done = False
         x, y  = x + dx, y + dy
         pd += 1
     return matrix
 
 # Проверка
-size = 5
-spiral = generate_spiral(size)
+size = 10
+spiral = spiralize(size)
 for row in spiral:
     print(row)
